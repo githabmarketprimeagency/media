8d7a6b6a067cb05449add4c34bf37adfc079782e
githabmarketprimeagency <githab@marketprimeagency.com>
2026-01-14 01:54:02 +0100

    Updated 4-generation_des_endpoints_pour_une_entite.txt

--- domain/src/main/java/com/microservice/media/domain/repository/RequestCheckRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.media.domain.repository;
-
-import com.microservice.media.domain.model.RequestCheck;
-import com.microservice.media.domain.model.pagination.RequestCheckPaginatedRepresentation;
-import com.microservice.media.domain.repository.base.CrudPaginatedRepository;
-
-public interface RequestCheckRepository extends CrudPaginatedRepository<RequestCheck, RequestCheckPaginatedRepresentation> {
-    Integer countInPeriod(String startDate, String endDate, String url, String method, String content, String userIp);
-}
